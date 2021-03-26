35bb332c99b733e3f1ec1e12d2f163815be4bd6f
Patrick Harrison <jahman@bellatlantic.net>
2021-03-26 11:22:08 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,24 @@
+filter_parameter_loggingdef greeting
+  puts "Hello World i am god"
+end
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+def say_greeting_six_times
+  puts "Hello World i am a chicken"
+end
+say_greeting_six_times
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("patrick", "jamaican")
+
+def greeting(name)
+  puts "Hello, #{name}!I love you"
+end
+greeting("petrona")
